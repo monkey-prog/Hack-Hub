b521cf0c2bb1949ea1d8148dfbed4f2805917d32
monkey-prog <afonsocristovao73@gmail.com>
2025-02-24 21:45:45 +0000

    Update script.lua

--- script.lua
+++ script.lua
@@ -28,9 +28,9 @@ local Window = Rayfield:CreateWindow({
       Subtitle = "Link in discord server",
       Note = "Join discord server from misc tab", -- Use this to tell the user how to get a key
       FileName = "ExampleHubKey", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"hi12"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = {"hello,hello1"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
 
